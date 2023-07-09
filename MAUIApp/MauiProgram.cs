03a47d72f396366a657d1daf50cc31cfd8fd342a
HIMELLAP\himel <himelhim31@gmail.com>
2023-07-09 10:54:09 +0600

    initial commit

---
+++ MAUIApp/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using MAUIApp.Data;
+using MudBlazor.Services;
+using MAUIApp.Helper;
+
+namespace MAUIApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMudServices();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7034") });
+        builder.Services.AddTransient<IHttpService, HttpService>();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
